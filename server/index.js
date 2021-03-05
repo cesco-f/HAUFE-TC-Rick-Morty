142a80dd100e64a5698554b3eeb32b030f0d6ad2
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-05 10:44:39 +0100

    fix: changed the default port in the server because it is the same React uses

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const db = require('./db');
 const router = require('./router');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
