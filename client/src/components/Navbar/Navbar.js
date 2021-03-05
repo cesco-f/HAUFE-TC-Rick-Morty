c61602dc9320414741c35c32499a50072225a266
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-05 19:34:58 +0100

    feat: implemented Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import LoginBtn from './LoginBtn/LoginBtn';
+import LogoutBtn from './LogoutBtn/LogoutBtn';
+import RegisterBtn from './RegisterBtn/RegisterBtn';
+
+import './Navbar.scss';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const token = useSelector((state) => state.token);
+
+  return (
+    <div className="Navbar">
+      <div className="Navbar-btns">
+        {token ? (
+          <LogoutBtn />
+        ) : (
+          <>
+            <LoginBtn />
+            <RegisterBtn />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
