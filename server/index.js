f071ae67aac98760e189377982a3385ceec6a234
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-05 10:09:31 +0100

    refactor: removed body-parser and added rule for console in the linter

--- server/index.js
+++ server/index.js
@@ -2,31 +2,29 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')();
+const cors = require('cors');
 const db = require('./db');
 const router = require('./router');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 app.use(router);
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.status(404).send('Sorry, not found 😞');
 });
 
 (async () => {
   try {
     await db.connect();
-    console.log('🏂 Successfully connected to MongoDB. 🏐');
+    console.log('🏂 Successfully connected to MongoDB. 🏐'); // eslint-disable-line no-console
     app.listen(PORT, (err) => {
       if (err) return console.log(err); // eslint-disable-line no-console
       console.log(`🏂 Server listening on port ${PORT} 🏐`); // eslint-disable-line no-console
     });
   } catch (error) {
-    console.error('Connection error', error);
+    console.error('Connection error', error); // eslint-disable-line no-console
   }
 })();
