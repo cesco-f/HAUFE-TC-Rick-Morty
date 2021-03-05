efc1a4b64da0830062a5d72c350b9f046a9d1199
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-05 18:28:04 +0100

    fix: minor fix in token reducer

--- client/src/store/reducers/token.js
+++ client/src/store/reducers/token.js
@@ -1,12 +1,12 @@
 import * as actionTypes from '../actions/actionTypes';
-const initialToken = undefined;
+const initialToken = null;
 
 export const tokenReducer = (state = initialToken, action) => {
   switch (action.type) {
     case actionTypes.SET_VALID_TOKEN:
       return action.payload;
     case actionTypes.SET_INVALID_TOKEN:
-      return undefined;
+      return null;
     default:
       return state;
   }
