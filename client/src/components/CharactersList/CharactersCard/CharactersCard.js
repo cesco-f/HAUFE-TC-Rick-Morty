ab526ac9a6370e8eae74dc22635f4b4e53f2d8b9
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-06 19:15:30 +0100

    feat: implemented CharactersCard component

--- client/src/components/CharactersList/CharactersCard/CharactersCard.js
+++ client/src/components/CharactersList/CharactersCard/CharactersCard.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import './CharactersCard.scss';
 
 function CharactersCard({ character }) {
-  return <div>{character.id}</div>;
+  const history = useHistory();
+
+  const goToDetails = () => {
+    history.push(`/character/${character.id}`);
+  };
+
+  return (
+    <div className="CharactersCard">
+      <div className="CharactersCard-name">{character.name}</div>
+      <div className="CharactersCard-image">
+        <img src={character.image} alt="character" />
+      </div>
+      <div className="CharactersCard-favList">
+        {character.favList ? 'In fav list' : 'Not in fav list'}
+      </div>
+      <div className="CharactersCard-button">
+        <button onClick={goToDetails}>Go to details</button>
+      </div>
+    </div>
+  );
 }
 
 export default CharactersCard;
