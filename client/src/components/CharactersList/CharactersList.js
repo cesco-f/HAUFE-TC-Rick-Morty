bd06e2eae7dc43082141689f1c83ed6e020aaa06
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-07 17:03:47 +0100

    refactor: removed useless fetch of characters from CharactersList component

--- client/src/components/CharactersList/CharactersList.js
+++ client/src/components/CharactersList/CharactersList.js
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getCharacters } from '../../store/actions/charactersActions';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import CharactersCard from './CharactersCard/CharactersCard';
 
 function CharactersList() {
-  const dispatch = useDispatch();
   const characters = useSelector((state) => state.characters);
-  const token = useSelector((state) => state.token);
-
-  useEffect(() => {
-    if (!characters) {
-      dispatch(getCharacters(token));
-    }
-  }, [characters, token, dispatch]);
 
   return (
     <div className="CharactersList">
