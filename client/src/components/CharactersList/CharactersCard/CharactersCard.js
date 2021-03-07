6ef47f15e76e82f97c03a6803403c4e9e5b70b05
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-07 11:57:08 +0100

    feat: the button for checking the details of a character replaced with Link from react router

--- client/src/components/CharactersList/CharactersCard/CharactersCard.js
+++ client/src/components/CharactersList/CharactersCard/CharactersCard.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-
-import Button from './../../UI/Button/Button';
+import { Link } from 'react-router-dom';
 
 import './CharactersCard.scss';
 
 function CharactersCard({ character }) {
-  const history = useHistory();
-
-  const goToDetails = () => {
-    history.push(`/character/${character.id}`);
-  };
-
   return (
-    <div className="CharactersCard">
-      <div className="CharactersCard-name">{character.name}</div>
-      <div className="CharactersCard-image">
-        <img src={character.image} alt="character" />
+    <Link to={`/character/${character.id}`}>
+      <div className="CharactersCard">
+        <div className="CharactersCard-name">{character.name}</div>
+        <div className="CharactersCard-image">
+          <img src={character.image} alt="character" />
+        </div>
+        <div className="CharactersCard-favList">
+          {character.favList ? 'In fav list' : 'Not in fav list'}
+        </div>
       </div>
-      <div className="CharactersCard-favList">
-        {character.favList ? 'In fav list' : 'Not in fav list'}
-      </div>
-      <div className="CharactersCard-button">
-        <Button text="Go to details" onClickCb={goToDetails} />
-      </div>
-    </div>
+    </Link>
   );
 }
 
