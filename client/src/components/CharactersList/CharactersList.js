6883ce31d24a152989c30baa3890b51d0d6b2aad
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-09 15:29:20 +0100

    refactor: moved the CharactersCard outside the CharactersList folder

--- client/src/components/CharactersList/CharactersList.js
+++ client/src/components/CharactersList/CharactersList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import CharactersCard from './CharactersCard/CharactersCard';
+import CharactersCard from './../CharactersCard/CharactersCard';
 
 function CharactersList() {
   const characters = useSelector((state) => state.characters);
