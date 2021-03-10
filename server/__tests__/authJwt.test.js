6d810087f6a454b753fcec947467366187546f61
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-10 16:59:18 +0100

    feat: added tests for the verifyToken function

--- server/__tests__/authJwt.test.js
+++ server/__tests__/authJwt.test.js
@@ -14,4 +14,18 @@ describe('verifyToken', () => {
     expect(spy).toHaveBeenCalledTimes(0);
     expect(res.status).toHaveBeenLastCalledWith(403);
   });
+
+  it('should return an error if the token is provided but it is not valid', () => {
+    const req = {
+      headers: { 'x-access-token': 'fake-token' },
+    };
+    const res = {
+      send: jest.fn(() => res).mockName('send'),
+      status: jest.fn(() => res).mockName('status'),
+    };
+    const spy = jest.fn();
+    verifyToken(req, res, spy);
+    expect(spy).toHaveBeenCalledTimes(0);
+    expect(res.status).toHaveBeenLastCalledWith(401);
+  });
 });
