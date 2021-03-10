2cef49544567c5f2e9424e838d2c8f1cfb77eac6
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-03-10 16:22:28 +0100

    feat: styled the NotFound component

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import peace from './../../assets/rick_morty_peace.jpeg';
+import { useHistory } from 'react-router-dom';
+
+import Button from './../UI/Button/Button';
 
 function NotFound() {
-  return <div>NotFound</div>;
+  const history = useHistory();
+
+  return (
+    <div className="NotFound">
+      <h1 className="NotFound-title">Sorry, not found</h1>
+      <img className="NotFound-image" src={peace} alt="not found" />
+      <Button text="Homepage" onClickCb={() => history.push('/')} />
+    </div>
+  );
 }
 
 export default NotFound;
